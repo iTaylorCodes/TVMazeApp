a7a8791a0c488324aef7034bfaad56a12d84ebce
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-04-23 21:27:49 -0700

    First vs complete

--- tvmaze.js
+++ tvmaze.js
@@ -2,7 +2,6 @@
  *     { id, name, summary, episodesUrl }
  */
 
-
 /** Search Shows
  *    - given a search term, search for tv shows that
  *      match that query.  The function is async show it
@@ -14,77 +13,79 @@
         id: <show id>,
         name: <show name>,
         summary: <show summary>,
-        image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
+        image: <an image from the show data, or a default image if no image exists, (image isn't needed until later)>
       }
  */
+
+const altImg = 'https://tinyurl.com/tv-missing';
+
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
+	let response = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+	let shows = response.data.map((result) => {
+		let show = result.show;
+		return {
+			id: show.id,
+			name: show.name,
+			summary: show.summary,
+			image: show.image ? show.image.medium : altImg
+		};
+	});
+
+	return shows;
 }
-
-
 
 /** Populate shows list:
  *     - given list of shows, add shows to DOM
  */
 
 function populateShows(shows) {
-  const $showsList = $("#shows-list");
-  $showsList.empty();
+	const $showsList = $('#shows-list');
+	$showsList.empty();
 
-  for (let show of shows) {
-    let $item = $(
-      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
+	for (let show of shows) {
+		let $item = $(
+			`<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
+          <img class="card-img-top" src="${show.image}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
            </div>
          </div>
        </div>
-      `);
+      `
+		);
 
-    $showsList.append($item);
-  }
+		$showsList.append($item);
+	}
 }
-
 
 /** Handle search form submission:
  *    - hide episodes area
  *    - get list of matching shows and show in shows list
  */
 
-$("#search-form").on("submit", async function handleSearch (evt) {
-  evt.preventDefault();
+$('#search-form').on('submit', async function handleSearch(evt) {
+	evt.preventDefault();
 
-  let query = $("#search-query").val();
-  if (!query) return;
+	let query = $('#search-query').val();
+	if (!query) return;
 
-  $("#episodes-area").hide();
+	$('#episodes-area').hide();
 
-  let shows = await searchShows(query);
+	let shows = await searchShows(query);
 
-  populateShows(shows);
+	populateShows(shows);
 });
-
 
 /** Given a show ID, return list of episodes:
  *      { id, name, season, number }
  */
 
 async function getEpisodes(id) {
-  // TODO: get episodes from tvmaze
-  //       you can get this by making GET request to
-  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
-
-  // TODO: return array-of-episode-info, as described in docstring above
+	// TODO: get episodes from tvmaze
+	//       you can get this by making GET request to
+	//       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
+	// TODO: return array-of-episode-info, as described in docstring above
 }
